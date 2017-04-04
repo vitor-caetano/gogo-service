313178d0ed27a4ed80484d04bf0a32ea0421cab9
Vitor Caetano <vitor.e.caetano@gmail.com>
2017-04-04 15:19:47 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import(
+	"os"
+	service "github.com/vitor-caetano/gogo-service/service"
+)
+
+func main() {
+	port := os.Getenv("Port")
+	if len(port) == 0 {
+		port = 3000
+	}
+
+	server := service.NewServer()
+	server.Run(":" + port)
+}
